ce887b58e3d4bfb85bf02176249bc553be54c179
bojanaveen152-netizen <bojanaveen152@gmail.com>
2026-09-04 11:18:13 +0100

    Implement input for string, integer, and float types
    Add input handling for string, integer, and float.

---
+++ Taking_Input_Day_5.py
@@ -0,0 +1,13 @@
+import math
+
+# s to store string
+# n to store integer
+# f to store float
+# ff  # To Store floor of float variable f
+
+s=input()
+n=int(input())
+f=float(input())
+print(s)
+print(n)
+print(int(f))
